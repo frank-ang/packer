105bfca9b420c540df2196d1b5f9e6b16248998c
Frank Ang <franklogic8@gmail.com>
2022-04-08 16:09:46 +0800

    sum total files size

--- pack.py
+++ pack.py
@@ -1,41 +1,31 @@
 import os,sys
 
-def to_delete():
-    for dirname, dirnames, filenames in os.walk('test'):
-        # editing the 'dirnames' list will stop os.walk() from recursing into there.
-        if '.git' in dirnames:
-            # don't go into any .git directories.
-            dirnames.remove('.git')
-
-        # print path to all subdirectories first.
-        for subdirname in dirnames:
-            print(os.path.join(dirname, subdirname))
-
-        # print path to all filenames.
-        for filename in filenames:
-            print(os.path.join(dirname, filename))
-
-
-def direc_tree_ory(p,j):
-    for i in os.listdir(str(p)):
-        try:
-            t=os.path.join(p,i)
-            if os.path.isdir(t):
-                
-                print(j+'#'+i)
-                direc_tree_ory(t,j+'-->')
-                print ('')
+def handle_directory(base_path, depth_string):
+    with os.scandir(path=base_path) as children:
+        total_bytes=0
+        for entry in children:
+            if entry.is_file():
+                # File
+                total_bytes += entry.stat().st_size
+                print(depth_string + entry.name + ":" + str(entry.stat().st_size))
+                # print("size:" + str(entry.stat().st_size))
+            elif entry.is_dir():
+                # Directory
+                print(depth_string+'#'+entry.name)
+                total_bytes += handle_directory(entry.path, depth_string+'-->')
+                # TODO... need a return value.
+                # e.g. total += handle_directory()
+                #   return total... ??
             else:
-                x=j+i
-                print(x)
-        except:
-            print('\n**Exception. **\n')
-        
+                print ("OTHER!")
+        return total_bytes
+
 k=''
-s="./test/origin"
-print('Path:' + s)
+base_path="/Users/frankang/lab/packer/test" # "./test/origin" # TODO: fix Hardcoding
+print('Scanning Path:' + base_path)
 try:
-    direc_tree_ory(s,k)
-except:
-    print('end of program')
-    pass
+    total_bytes=handle_directory(base_path,k)
+    print("TOTAL size bytes:"+str(total_bytes))
+except Exception as e:
+    print(e)
+    raise
