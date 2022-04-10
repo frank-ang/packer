8c24162a13073840aa721b5697ec8a7515613342
Frank Ang <franklogic8@gmail.com>
2022-04-11 00:57:54 +0800

    skip excluded files

--- pack.py
+++ pack.py
@@ -1,14 +1,16 @@
 from doctest import UnexpectedException
-import os,sys
+import os,re
 from unicodedata import name
 
 class PackConfig:
     bin_max_bytes = 100
     base_path = "."
     staging_path = "./staging"
+    exclude_patterns = ""
     def __init__(self, base_path, staging_path):
         self.base_path = base_path
         self.staging_path = staging_path
+        self.exclude_patterns = [".DS_Store"]
 
 class Bin:
     bin_id = 0
@@ -17,7 +19,9 @@ class Bin:
         self.bin_id = bin_id
         self.bin_size = 0
     def add(self, filesize):
+        print("Bin:{}, size:{}, add:{}".format(self.bin_id, self.bin_size, filesize))
         self.bin_size += filesize
+        return self.bin_size
     def bin_name(self):
         return "CAR{}".format(self.bin_id)
 
@@ -32,12 +36,21 @@ def handle_directory(path, config, bin_list):
     for entry in children:
         if entry.is_file():
             # File
+
+            # Skip excluded files
+            # Using join regex + loop + re.match()
+            pattern = '(?:% s)' % '|'.join(config.exclude_patterns)
+            if re.match(pattern, entry.name):
+                continue
+
             file_size = entry.stat().st_size
+
             # TODO:
             # 1. Split large files. 
             # 2. Encrypt files. 
             # 3. Detect when max size is reached, change to new target base dir.
             # 4. Move files to target base.
+            print("testing if bin size:{} + filesize:{} > {}".format(cur_bin.bin_size, file_size, config.bin_max_bytes))
             if (cur_bin.bin_size + file_size) > config.bin_max_bytes:
                 print("Bin Increment! ", cur_bin.bin_size, "+", file_size, " > ", config.bin_max_bytes)
                 next_bin = Bin(cur_bin.bin_id + 1)
