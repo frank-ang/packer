f5b474638947dfe56e9c04077395381aafa0ec8d
Frank Ang <franklogic8@gmail.com>
2022-04-11 00:32:13 +0800

    WIP bin increment

--- pack.py
+++ pack.py
@@ -1,32 +1,72 @@
+from doctest import UnexpectedException
 import os,sys
+from unicodedata import name
 
-def handle_directory(base_path, depth_string):
-    with os.scandir(path=base_path) as children:
-        total_bytes=0
-        for entry in children:
-            if entry.is_file():
-                # File
-                total_bytes += entry.stat().st_size
-                print(depth_string + entry.name + ":" + str(entry.stat().st_size))
-                # TODO:
-                # 1. Split large files. Move to target base.
-                # 2. Encrypt files. Move to target base.
-                # 3. Detect when max size is reached, change to new target base dir.
-            elif entry.is_dir():
-                # Directory
-                print(depth_string+'#'+entry.name)
-                total_bytes += handle_directory(entry.path, depth_string+'-->')
+class PackConfig:
+    bin_max_bytes = 100
+    base_path = "."
+    staging_path = "./staging"
+    def __init__(self, base_path, staging_path):
+        self.base_path = base_path
+        self.staging_path = staging_path
+
+class Bin:
+    bin_id = 0
+    bin_size = 0
+    def __init__(self, bin_id):
+        self.bin_id = bin_id
+        self.bin_size = 0
+    def add(self, filesize):
+        self.bin_size += filesize
+    def bin_name(self):
+        return "CAR{}".format(self.bin_id)
+
+def handle_directory(path, config, bin_list):
+
+    cur_bin = bin_list[-1]
+
+    with os.scandir(path) as iterator:
+        children = list(iterator)
+    children.sort(key= lambda x: x.name)
+
+    for entry in children:
+        if entry.is_file():
+            # File
+            file_size = entry.stat().st_size
+            # TODO:
+            # 1. Split large files. 
+            # 2. Encrypt files. 
+            # 3. Detect when max size is reached, change to new target base dir.
+            # 4. Move files to target base.
+            if (cur_bin.bin_size + file_size) > config.bin_max_bytes:
+                print("Bin Increment! ", cur_bin.bin_size, "+", file_size, " > ", config.bin_max_bytes)
+                next_bin = Bin(cur_bin.bin_id + 1)
+                next_bin.add(file_size)
+                bin_list.append(next_bin)
+                cur_bin=next_bin
             else:
-                print ("OTHER!")
-        print(base_path, ", Total:",total_bytes)
-        return total_bytes
+                cur_bin.add(file_size)
+            print(entry.path, ":FileSize:", file_size, ", BinSize:", cur_bin.bin_size)
+            file_staging_path = os.path.join(config.staging_path, 
+                                cur_bin.bin_name(), 
+                                os.path.relpath(entry.path,config.base_path))
+            file_staging_path = os.path.normpath(file_staging_path)
+            print("   ... copy to staging:", file_staging_path)
+        elif entry.is_dir():
+            # Directory
+            print(entry.path, "... directory")
+            handle_directory(entry.path, config, bin_list)
+            # subdirectories may have added new bins
+        else:
+            raise UnexpectedException("Entry is not dir or file type.")
 
-k=''
-base_path="/Users/frankang/lab/packer/test" # "./test/origin" # TODO: fix Hardcoding
+base_path="./test/origin" # TODO: fix Hardcoding
+staging_path="./test/staging"  # TODO: fix Hardcoding
 print('Scanning Path:' + base_path)
 try:
-    total_bytes=handle_directory(base_path,k)
-    print("TOTAL size bytes:"+str(total_bytes))
+    config = PackConfig(base_path, staging_path)
+    bin_list = [Bin(0)]
+    handle_directory(base_path, config, bin_list)
 except Exception as e:
     print(e)
     raise
